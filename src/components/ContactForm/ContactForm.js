3a6841aba8f94a60033ca9cc174de06f18c0c4ef
mikealexcd <mikealexcd@gmail.com>
2021-11-04 18:06:49 -0400

    add contact form

---
+++ src/components/ContactForm/ContactForm.js
@@ -0,0 +1,26 @@
+import styles from "./ContactForm.module.scss";
+
+function ContactForm() {
+
+    return (
+        
+        <div className={styles.container}>
+            <div className={styles.wrapper}>
+                <div className={styles.column}>
+                    <h2 className={styles.heading}>Would you like to reach out?</h2>
+                    <p className={styles.text}>Get in touch if you're interested in chatting with me!</p>
+                </div>
+                <div className={styles.column}>
+                    <form className={styles.form}>
+                        <input className={styles.email} type="email" name="email" placeholder="Email"></input>
+                        <textarea className={styles.message} type="textarea" name="message" placeholder="Message"></textarea>
+                        <input className={styles.submit} type="submit" name="submit"></input>
+                    </form>
+                </div>
+            </div>
+        </div>
+        
+    )
+}
+
+export default ContactForm;
